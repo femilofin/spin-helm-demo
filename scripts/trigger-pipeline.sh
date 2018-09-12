a305902f8bda5b4059a763e2e0ea5c4859d71157
Oluwafemi Olofintuyi <oluwafemi.t.olofintuyi@gmail.com>
2018-09-12 14:17:30 +0100

    fix typo

--- scripts/trigger-pipeline.sh
+++ scripts/trigger-pipeline.sh
@@ -3,7 +3,7 @@
 SPINNAKER_API=https://spinnaker.demo.armory.io/api/webhooks/webhook/spin-helm-demo
 CHART_VERSION=`cat chart/spin-helm-demo/Chart.yaml | grep version | awk '{print $2}'`
 CHART_NAME=`cat chart/spin-helm-demo/Chart.yaml | grep name | awk '{print $2}'`
-CHART_GIT_API=`https://api.github.com/repos/femilofin/helm-charts/spin-helm-demo-${CHART_VERSION}.tgz`
+CHART_GIT_API="https://api.github.com/repos/femilofin/helm-charts/spin-helm-demo-${!CHART_VERSION}.tgz"
 
 curl -L -vvv -X POST \
 		-k \
