d0921e43c54e76a57efd4dfccf9de605aa64fec8
Oluwafemi Olofintuyi <oluwafemi.t.olofintuyi@gmail.com>
2018-09-12 15:22:09 +0100

    bump up version

--- scripts/trigger-pipeline.sh
+++ scripts/trigger-pipeline.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -ex
 
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  exit 0
-fi
-
 SPINNAKER_API=https://spinnaker.demo.armory.io/api/webhooks/webhook/spin-helm-demo
 CHART_VERSION=`cat chart/spin-helm-demo/Chart.yaml | grep version | awk '{print $2}'`
 CHART_NAME=`cat chart/spin-helm-demo/Chart.yaml | grep name | awk '{print $2}'`
-CHART_GIT_API="https://api.github.com/repos/femilofin/helm-charts/spin-helm-demo-${!CHART_VERSION}.tgz"
+CHART_GIT_API="https://api.github.com/repos/femilofin/helm-charts/spin-helm-demo-${CHART_VERSION}.tgz"
 
 curl -L -vvv -X POST \
 		-k \
 		-H"Content-Type: application/json" ${SPINNAKER_API} \
 		-d '{"artifacts": [{"type": "github/file", "name": "${CHART_NAME}", "reference":
-"${CHART_GIT_API}", "kind": "default.github"}]}'
+${CHART_GIT_API}, "kind": "default.github"}]}'
